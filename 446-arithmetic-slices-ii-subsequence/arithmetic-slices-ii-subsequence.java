63e48412758f6970fa6fe05fc6944e69260a6622
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-01-07 18:42:45 +0530

    Time: 33 ms (100.00%) | Memory: 52.4 MB (93.60%) - LeetSync

---
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        HashMap<Long, List<Integer>> storage = new HashMap<>();
+        int size = nums.length;
+        int[][] dp = new int[size][size];
+
+        int result = 0;
+        for(int index=0; index<size; index++){
+            long num = nums[index];
+            storage.computeIfAbsent(num, k -> new ArrayList<>()).add(index);}
+
+        for(int row=1; row<size; row++){
+            for(int col=row+1; col<size; col++){
+                long prev = 2L * nums[row] - nums[col];
+                if (prev > Integer.MAX_VALUE || prev < Integer.MIN_VALUE)
+                    continue;
+                if(storage.containsKey(prev) ){
+                    for(int k : storage.get(prev)){
+                        if(k < row){
+                            dp[row][col] += dp[k][row] + 1;
+                        }else{
+                            break;
+                        }
+                    }
+                }
+                result += dp[row][col];
+            }
+        }
+
+        return result;
+    }
+}
