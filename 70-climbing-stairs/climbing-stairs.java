c98393e3a0c4f5f71d9a4123408e5b4efdc6a871
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-07-17 21:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (7.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        // If the N is less than 2 | No.of ways to reach will be N.
-        if(n<=2) return n;
-
-        // Initialize variables to keep track of previous two steps.
-        int prev = 1, B_prev = 2;
-        // Current Step Count based on previous Two steps.
-        int current = 0;
-
-        for(int loop=1; loop<n-1; loop++){
-            current = prev + B_prev; // Calculate current steps
-            // Update previous steps for next iteration
-            prev = B_prev;
-            B_prev = current;
+        int prev=0;
+        int prev1=1;
+        for(int i=2;i<=n;i++)
+        {
+            int curr=prev+prev1;
+            prev=prev1;
+            prev1=curr;
         }
+        return prev1+prev;
 
-        return current;
     }
 }
